de3897dd9bf68dc07b246151c5ce282bd8085ad5
DiegoDuran <diego.duran@gmail.com>
2021-03-31 15:55:07 -0300

    cambios en ejercicio 176

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,6 +1,20 @@
 import React from 'react'
+import { Redirect, useParams } from 'react-router'
+import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
+
+    const {heroeId}= useParams();
+
+    const heroe= getHeroById(heroeId);
+
+    //si no encontroe.. redirijo
+    if (!heroe) {
+        return <Redirect to="/"/> 
+    }
+
+    const {superhero, publisher, alter_ego, first_appearance, characters}= heroe;
+
     return (
         <div>
             <h1>HeroScreen</h1>
